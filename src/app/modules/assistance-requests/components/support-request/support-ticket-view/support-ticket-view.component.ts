141c4ab78581593659a5a3621bce69ff5e46979e
VISHNUPRABHU-170 <vishnurr1708@gmail.com>
2025-03-02 15:20:01 +0530

    Implemented Ticket Delete Functionalities.

--- src/app/modules/assistance-requests/components/support-request/support-ticket-view/support-ticket-view.component.ts
+++ src/app/modules/assistance-requests/components/support-request/support-ticket-view/support-ticket-view.component.ts
@@ -1,6 +1,6 @@
 import { updateButtonConfig } from './../../feature-request/feature-ticket-view/config';
 import { SupportRequestService } from './../../../services/support-request/support-request.service';
-import { Component, DestroyRef, OnInit } from '@angular/core';
+import { Component, DestroyRef, OnInit, ViewEncapsulation } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbar } from '@angular/material/toolbar';
 import { IconComponent } from '../../../../core/components/icon/icon.component';
@@ -8,18 +8,20 @@ import { backIconConfig, previewLinkConfig, progressBarConfig } from './config';
 import { ActivatedRoute } from '@angular/router';
 import { SupportTicketModel } from '../../../models/support-ticket.model';
 import { DatePipe } from '@angular/common';
-import { ButtonComponent } from '../../../../core/components/button/button.component';
 import { ChipComponent } from '../../../../core/components/chip/chip.component';
 import { NgxSkeletonLoaderComponent } from 'ngx-skeleton-loader';
 import { ChipComponentModel } from '../../../../core/components/chip/chip.component.model';
 import { NavigationService } from '../../../../core/services/navigation/navigation.service';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-support-ticket-view',
   standalone: true,
-  imports: [MatToolbar, MatCardModule, IconComponent, DatePipe, ButtonComponent, NgxSkeletonLoaderComponent, ChipComponent],
+  imports: [MatToolbar, MatCardModule, IconComponent, DatePipe, NgxSkeletonLoaderComponent, ChipComponent, MatMenuModule, MatProgressBarModule],
   templateUrl: './support-ticket-view.component.html',
   styleUrl: './support-ticket-view.component.scss',
+  encapsulation: ViewEncapsulation.None
 })
 export class SupportTicketViewComponent implements OnInit {
   backIconConfig = backIconConfig;
@@ -29,6 +31,8 @@ export class SupportTicketViewComponent implements OnInit {
 
   ticketID!: string;
   ticketData!: SupportTicketModel;
+
+  isRequestingSending = false;
 
   skeletonLoaderHeaderConfig = { width: '250px', height: '40px', 'border-radius': '10px', 'background-color': '#00000029' };
   skeletonLoaderConfig = { width: '250px', height: '20px', 'border-radius': '10px' }
@@ -41,12 +45,19 @@ export class SupportTicketViewComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    this.subscribeToIsRequestingSending();
     this.ticketID = this.route.snapshot.queryParamMap.get('id')!;
     const Subscription = this.supportRequestService.getTicket(this.ticketID).subscribe({
       next: response => this.ticketData = response.data
     });
     this.destroyRef.onDestroy(() => {
       Subscription?.unsubscribe();
+    });
+  }
+
+  subscribeToIsRequestingSending(): void {
+    this.supportRequestService.isRequestingSending.subscribe((isRequestingSending) => {
+      this.isRequestingSending = isRequestingSending;
     });
   }
 
@@ -57,6 +68,10 @@ export class SupportTicketViewComponent implements OnInit {
   onUpdate(): void {
     const queryParams = { id: this.ticketID };
     this.navigationService.navigate('/partner-portal/assistance-requests/support-form', queryParams);
+  }
+
+  onDelete(): void {
+    this.supportRequestService.deleteTicket(this.ticketID);
   }
 
   prepareChipConfig(label: string): ChipComponentModel {
